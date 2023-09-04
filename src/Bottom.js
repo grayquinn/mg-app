4e0e2bf8c45093347da9e49841e780110449e282
Grayson Quinn <graysonquinn2@gmail.com>
2023-09-04 13:44:23 -0500

    added buttons

--- src/Bottom.js
+++ src/Bottom.js
@@ -1,25 +1,77 @@
-
-
+import React, {useState} from 'react';
 
 const Bottom = () => {
-    
+    const [content, setContent] = useState({
+        personnel: 'Jenny Doe',
+        position: 'Lead Hammerer',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+    });
+
+    const handleButtonClick = (newContent) => {
+        setContent(newContent);
+    };
+
     return (
         <div className="bottom">
             <div className='wrapperB'>
-            
                 <div className='text'>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    <p>{content.text}</p>
                 </div>
                 <div className="info">
-                    <div className="Personnel">Jenny Doe</div>
-                    <div className="Position">Lead Hammerer</div>
-                </div>
-                <div className="buttons">
-
-                </div>
+                    <div className="Personnel">{content.personnel}</div>
+                    <div className="Position">{content.position}</div>
+            </div>
+            <div className="buttons">
+                <button onClick={() => handleButtonClick({ personnel: 'John Doe', position: 'Manager', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam' })} className="button">Button 1</button>
+                <button onClick={() => handleButtonClick({ personnel: 'Jane Doe', position: 'Designer', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus facilisis vestibulum lorem, ac euismod tortor. Fusce eget gravida ex. Phasellus nec turpis eu sapien tincidunt efficitur.' })} className="button">Button 2</button>
+                <button onClick={() => handleButtonClick({ personnel: 'Jim Doe', position: 'Developer', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in lectus nec metus gravida tempus. Vestibulum ac vestibulum velit. Maecenas vel feugiat mi, ut fringilla metus.' })} className="button">Button 3</button>
             </div>
         </div>
-    )
+    </div>
+    );
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default Bottom
